71b99edfcdfb909786954310e43515372a8b1d49
mlahiru1 <164457106+mlahiru1@users.noreply.github.com>
2024-05-09 23:41:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pyttsx3 as p
 
 engine = p.init()
-
-engine.say("hello world.My name is nova")
+rate = engine.getProperty('rate')
+engine.setProperty('rate',180)
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+print(voices)
+engine.say("hello world. My name is nova")
 engine.runAndWait()
